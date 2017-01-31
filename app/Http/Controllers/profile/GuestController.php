cc76fb99ae1b7d8e626b564d90f7098709d80bd9
gauravpant99 <gauravpant99@gmail.com>
2017-01-31 11:17:08 -0500

    Updated the guest controller to redirect to home page is the user is logged in

--- app/Http/Controllers/profile/GuestController.php
+++ app/Http/Controllers/profile/GuestController.php
@@ -9,6 +9,11 @@ use App\Http\Requests;
 use App\Http\Controllers\Controller;
 class GuestController extends Controller
 {
+    public function __construct()
+    {
+
+        $this->middleware('guest');
+    }
 
 
     public function index()
