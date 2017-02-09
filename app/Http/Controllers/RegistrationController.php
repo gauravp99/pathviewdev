378ce2f689f8596e13c84b81b3996ab4dae9a9ec
gauravpant99 <gauravpant99@gmail.com>
2017-02-09 11:11:11 -0500

    Registration Controller: Removed the typo of activation to activated when the user is create .Added the debug code to handle the scenarios when the user is created with activated 0 and confirmation code null value

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -8,6 +8,7 @@ use Redirect;
 use Mail;
 use Hash;
 use App\User;
+use Exception;
 #use Illuminate\Support\Facades\Redirect;
 #class RegistrationController extends \BaseController {
 class RegistrationController extends Controller {
@@ -43,21 +44,36 @@ class RegistrationController extends Controller {
         {
             return Redirect::back()->withInput()->withErrors($validator);
         }
-        $confirmation_code = str_random(30);
-        User::create([
-            #'username' => Input::get('username'),
-            'name' => Input::get('name'),
-            'email' => Input::get('email'),
-            'password' => Hash::make(Input::get('password')),
-            'confirmation_code' => $confirmation_code,
-	    'activation' => 0
-        ]);
-        Mail::send('emails.verify', compact('confirmation_code'), function($message) {
-            $message->to(Input::get('email'), Input::get('name'))->subject('[Pathview] Please verify your email address, '.Input::get('name'));
-        });
-        #Flash::message('Thanks for signing up! Please check your email and follow the instructions to complete the sign up process');
-        return redirect('/')->with('message', 'Thanks for signing up! Please check your email and follow the instructions to complete the sign up process.');
-        #return Redirect::home()->with('message', 'Thanks for signing up! Please check your email and follow the instructions to complete the sign up process');
+	try {
+           $confirmation_code = str_random(30);
+           User::create([
+               'name' => Input::get('name'),
+               'email' => Input::get('email'),
+               'password' => Hash::make(Input::get('password')),
+               'confirmation_code' => $confirmation_code,
+	       'activated' => 0
+           ]);
+           Mail::send('emails.verify', compact('confirmation_code'), function($message) {
+               $message->to(Input::get('email'), Input::get('name'))->subject('[Pathview] Please verify your email address, '.Input::get('name'));
+           });
+           return redirect('/')->with('message', 'Thanks for signing up! Please check your email and follow the instructions to complete the sign up process.');
+	}
+	catch (\Exception $e) {
+            ##The below code is added for debug purpose to
+	    # find the root cause of the issue for the user with 
+	    # activated -> 0 and confirmation_code -> null.
+	    # ToDo: Remove this code once the root cause of the issue is identified.
+	    error_log("Caught Exception while creating a new user");
+	    error_log($e->getMessage());
+	    error_log("Confirmation Code: $confirmation_code ");
+	    $name=Input::get('name');
+	    $file=fopen('data/register_error.txt', 'a');
+	    fwrite($file, $e->getMessage());
+	    fwrite($file, "\n Confirmation Code: $confirmation_code user : $name \n");
+	    fclose($file);
+            return redirect('/')->with('message', 'Error while signing up. Please register your acccount again.');
+
+	}
     }
     /**
      * Attempt to confirm a users account.
@@ -73,19 +89,14 @@ class RegistrationController extends Controller {
         if( ! $confirmation_code)
         {
             return redirect('/')->with('message', 'You have successfully verified your account 1');
-            #return Redirect::home();
         }
         $user = User::whereConfirmationCode($confirmation_code)->first();
-	$name = $user->name;
-	$email = $user->email;
-	error_log('user------------');
-	error_log($email);
         if ( ! $user)
         {
-            #return Redirect::home();
-            #return redirect('/auth/login')->with('message', 'You have successfully verified your account. You can now login.');
             return redirect('/')->with('message', 'This account has already been verified successfully.');
         }
+	$name = $user->name;
+	$email = $user->email;
         $user->activated = 1;
         $user->confirmation_code = null;
         $user->save();
@@ -93,7 +104,6 @@ class RegistrationController extends Controller {
         Mail::send('emails.welcome', $data1, function($message) use ($email, $name){
             $message->to($email, $name)->subject('[Pathview] Welcome to Pathview, '.$name);
         });
-        #$user->save();
         return redirect('/')->with('message', 'You have successfully verified your account. You can now login.');
     }
 }
