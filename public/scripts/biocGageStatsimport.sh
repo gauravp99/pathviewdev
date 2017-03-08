68ebf28f11b772028a28501ef94eee3111417184
gauravpant99 <gauravpant99@gmail.com>
2017-03-08 12:43:24 -0500

    Updated the gitignore to remove the biocStatsimport biocGageStatsimport from the ignore list. Updated the script not to expose the  DB credentials

---
+++ public/scripts/biocGageStatsimport.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#This script is for getting the stats from bioc gage web site and upload to pathway database every day morning 2:30
+#Author: Gaurav P
+#Script usage: Run the script from public directory without any parameter ./scripts/biocGageStatsimport.sh 
+
+##Check for the environment file. 
+
+if [ ! -f ../.env ]
+then
+   echo "The environment file doesn't exist.. Exiting !!"
+   exit 1
+fi
+
+source ../.env
+if [ -z $DB_USERNAME ] || [ -z $DB_PASSWORD ] || [ -z $DB_DATABASE ]
+then
+   echo "Database information not found. Exiting ...!!"
+   exit 1
+fi
+MYSQL="mysql -u$DB_USERNAME -p$DB_PASSWORD $DB_DATABASE"
+
+wget -q  "https://bioconductor.org/packages/stats/bioc/gage/gage_stats.tab"
+
+if [ $? -ne 0 ]
+then
+  echo "Error will fetching the stats from bioc gage website. Exiting..."
+  exit 1
+fi
+
+files=('gage_stats.tab')
+if [ -f gage_stats.tab ]
+then
+   for file in "${files[@]}"
+   do
+      tail -n +2 $file |grep -v all |  while read line
+      do
+         year=`echo $line | tr -s " "  | awk -F " " '{print $1}'`
+         month=`echo $line| tr -s " "  | awk -F " " '{print $2}'`
+         ipadd=`echo $line | tr -s " "  | awk -F " " '{print $3}'`
+         downloads=`echo $line | tr -s " "  | awk -F " " '{print $4}'`
+         #echo $year,$month,$ip,$downloads
+         if [ $downloads -ne 0 ]
+         then
+           echo "delete from biocGagestatistic where month = '$month' and year = '$year';"|$MYSQL
+           echo "insert into biocGagestatistic values('$month','$year',$ipadd,$downloads);" |$MYSQL
+         fi
+      done
+   done
+   rm gage_stats.tab
+else
+   echo "Bioc Gage Stats file doesn't exist ..Exiting"
+   exit 1
+fi
---
+++ public/scripts/biocStatsimport.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+#This script is for getting the stats from bioc web site and upload to pathway database every day morning 2:30
+#Author: Gaurav P
+#Script usage: Run the script from public folder without any parameter ./scripts/biocStatsimport.sh 
+#This script is run from the schedule job of laravel
+
+##Check for the environment file 
+
+if [ ! -f ../.env ]
+then
+   echo "The environment file doesn't exist.. Exiting !!"
+   exit 1
+fi
+
+source ../.env
+if [ -z $DB_USERNAME ] || [ -z $DB_PASSWORD ] || [ -z $DB_DATABASE ]
+then
+   echo "Database information not found. Exiting ...!!"
+   exit 1
+fi
+
+MYSQL="mysql -u$DB_USERNAME -p$DB_PASSWORD $DB_DATABASE"
+
+wget -q "https://bioconductor.org/packages/stats/bioc/pathview/pathview_stats.tab"
+if [ $? -ne 0 ]
+then
+	echo "Error will fetching the stats from bioc website. Exiting..."
+	exit 1
+fi
+
+files=('pathview_stats.tab')
+
+
+
+if [ -f pathview_stats.tab ]
+then
+   for file in "${files[@]}"
+   do
+      tail -n +2 $file |grep -v all |  while read line
+      do
+         year=`echo $line | tr -s " "  | awk -F " " '{print $1}'`
+         month=`echo $line| tr -s " "  | awk -F " " '{print $2}'`
+         ipadd=`echo $line | tr -s " "  | awk -F " " '{print $3}'`
+         downloads=`echo $line | tr -s " "  | awk -F " " '{print $4}'`
+         if [ $downloads -ne 0 ]
+         then
+           echo "delete from biocStatistics where month = '$month' and year = '$year';"|$MYSQL
+	   echo "insert into biocStatistics values('$month','$year',$ipadd,$downloads,now(),now());" |$MYSQL
+         fi
+      done
+    done
+    #rm pathview_2016_stats.tab pathview_2015_stats.tab
+    rm pathview_stats.tab
+else
+   echo "Stats file doesn't exist ..Exiting"
+   exit 1
+fi
