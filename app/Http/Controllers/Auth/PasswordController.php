c3435712f3ebc52cda0d6ebdd8c54f437036302c
gauravpant99 <gauravpant99@gmail.com>
2017-03-22 16:52:43 -0400

    Author : Gaurav P Date: 03/22/2017 Auth Password Controller is added with a new function that has been partially taken from Auth package.To avoid changes in the vendor directory , it has been added here.Before sending password reset mail to user, check if the user's account is activated, if not then re send the activation mail to the user for activating the account.

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -4,7 +4,9 @@ use App\Http\Controllers\Controller;
 use Illuminate\Contracts\Auth\Guard;
 use Illuminate\Contracts\Auth\PasswordBroker;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Http\Request;
 use Mail;
+use DB;
 class PasswordController extends Controller
 {
 
@@ -20,6 +22,42 @@ class PasswordController extends Controller
     */
 
     use ResetsPasswords;
+    ##This function has been re-written and partially taken from Auth vendor.
+    ##To avoid changes in the vendor directory , it has been added here.
+    ##Before sending password reset mail to user, check if the user's account is
+    #activated, if not then re send the activation mail to the user for activating 
+    #the account
+    public function postEmail(Request $request)
+    {
+    	$this->validate($request, ['email' => 'required|email']);
+    	$user_check = $this->passwords->getUser($request->only('email'));
+    	if (!$user_check->activated) 
+    	{
+    	      $confirmation_code=$user_check->confirmation_code;
+    	      $email=$user_check->email;
+    	      $name=$user_check->name;
+                  $data['name'] = $name;
+                  $data['email'] = $email;
+                  Mail::send('emails.verify', compact('confirmation_code'), function($message) use ($data) {
+                  $message->to($data['email'], $data['name'])->subject("[Pathview] Please verify your email address before resetting your password, ".$data['name']);
+                });
+               return back()->with('status', 'Your account is not activated. Please activate it first. An activation code has been sent to your email.');
+            }
+    
+    	$response = $this->passwords->sendResetLink($request->only('email'), function($m)
+    	{
+    		$m->subject($this->getEmailSubject());
+    	});
+    
+    	switch ($response)
+    	{
+    		case PasswordBroker::RESET_LINK_SENT:
+    			return redirect()->back()->with('status', trans($response));
+    
+    		case PasswordBroker::INVALID_USER:
+    			return redirect()->back()->withErrors(['email' => trans($response)]);
+    	}
+    }
 
     /**
      * Create a new password controller instance.
@@ -28,12 +66,12 @@ class PasswordController extends Controller
      * @param  \Illuminate\Contracts\Auth\PasswordBroker $passwords
      * @return void
      */
+
     public function __construct(Guard $auth, PasswordBroker $passwords)
     {
         
         $this->auth = $auth;
         $this->passwords = $passwords;
-
         $this->middleware('guest');
     }
 
